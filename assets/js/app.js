c6de8d9ba70ad53af0e947eb72b568e0c923c212
playniuniu <playniuniu@gmail.com>
2016-03-06 15:29:08 +0800

    add messagebox history feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 
 // 定义全局变量
 var $$ = Framework7.$;
-var gApp, mainView;
+var gApp, mainView, indexPageContainer;
 
 init_chat_box();
 
@@ -11,6 +11,8 @@ function init_chat_box() {
 
     // 初始化 Framework7 APP
     gApp = new Framework7({
+        // Disabel init
+        init: false,
 
         // Init model name
         modalTitle: 'drchat',
@@ -35,6 +37,9 @@ function init_chat_box() {
     // 初始化 page event
     initPageEvent();
     
+    // Init app
+    gApp.init();
+    
     // 检查登陆情况
     if (checkLoginStatus() === null) {
         mainView.router.loadPage("login.html");
@@ -42,16 +47,69 @@ function init_chat_box() {
 }
 
 function initPageEvent() {
+    // 处理主页
+    gApp.onPageInit('index', function (page) {
+        indexPageContainer = $$(page.container);
+        initIndexPageEvent(indexPageContainer);
+    });
+    
+    // 处理登录页
     gApp.onPageInit('login', function (page) {
         var pageContainer = $$(page.container);
         processLogin(pageContainer, mainView, gApp);
     });
     
+    gApp.onPageBack('login', function(page) {
+        initIndexPageEvent(indexPageContainer);
+    });
+    
+    // 处理聊天页
     gApp.onPageInit('chat', function(page) {
-       init_chat_page(gApp); 
+        init_chat_page(gApp);
     });
     
     gApp.onPageBack('chat', function(page) {
         disconnect_remote();
     });
+}
+
+// 页面响应
+function initIndexPageEvent(pageContainer) {
+    
+    var $logout_btn = pageContainer.find('#logout-btn');
+    var $nick_name= pageContainer.find('#nick-name');
+    var $save_btn = pageContainer.find('#save-btn');
+    var $login_name = pageContainer.find('#login-name');
+    
+    // 设置登录名
+    $login_name.val(checkLoginStatus());
+    
+    // 处理退出按钮
+    $logout_btn.on('click', function (event) {
+        setLogout();
+    });
+    
+    // 处理用户昵称
+    $nick_name.on('input', function (event) {
+        $save_btn.removeClass('disabled');
+        if($nick_name.val() === '') {
+            $save_btn.addClass('disabled');
+        }
+    });
+    
+    // 处理保存按钮
+    $save_btn.on('click', function(event) {
+        $save_btn.addClass('disabled');
+    });
+}
+
+// 更新消息列表
+function updateMessageList(data) {
+    var parse_data = JSON.parse(data);
+    if(parse_data.status !== 'ok') {
+        return false;
+    }
+    
+    var $message_list = indexPageContainer.find('#message-list');
+    var $message_template = $$('script#message-template').html();
 }
--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,12 +1,15 @@
 "use strict";
 
 // 定义变量
+var socket_port = 3000;
 var local_socket = null;
-var chat_page_messages, chat_page_messagebar, conversationStarted;
+var chat_page_messages, chat_page_messagebar;
+var current_chat_user, conversationStarted;
 
 // 初始化 chat 页面
 function init_chat_page(gApp) {
-    // 初始化 conversationStarted
+    // 初始化变量
+    current_chat_user = 'aws';
     conversationStarted = false;
 
     // 初始化 Message 对话框
@@ -23,12 +26,14 @@ function init_chat_page(gApp) {
     // 初始化消息处理函数
     handle_local_message();
     handle_remote_message();
+    
+    // 更新消息历史
+    ajaxHistroyMessage(checkLoginStatus(), update_message_history);
 }
 
 // 连接 socket io
 function connect_to_remote() {
     if(local_socket === null) {
-        var socket_port = 3000;
         var socket_url = "http://" + document.location.hostname + ":" + socket_port + '/drchat';
         local_socket = io(socket_url);
         console.log("Connected to " + socket_url);
@@ -62,7 +67,7 @@ function handle_local_message() {
 
         var messageSend = {
             from: checkLoginStatus(),
-            to: "admin",
+            to: current_chat_user,
             messageFormat: "text",
             messageTime: Date.now(),
             messageBody: messageText
@@ -118,4 +123,45 @@ function update_message_box(messageJSON, messageType) {
 
     // 更新会话flag
     conversationStarted = true;
+}
+
+function update_message_history(data) {
+    var recv_msg = JSON.parse(data);
+    if(recv_msg.status !== 'ok') {
+        return;
+    }
+    
+    var msg_list = recv_msg.data;
+    
+    // 读取的时候消息是按从新到旧排列的，因此需要倒序
+    msg_list = msg_list.reverse();
+    
+    for(var i=0; i< msg_list.length; i++) {
+        var el = JSON.parse(msg_list[i]);
+        
+        // 检测本人发送
+        if(el.from === checkLoginStatus()) {
+            update_message_box(msg_list[i], "sent");
+        }
+        
+        // 检测本人接收
+        if(el.to === checkLoginStatus()) {
+            update_message_box(msg_list[i], "received");
+        }
+    }
+}
+
+// 获取历史聊天记录, 并回调
+function ajaxHistroyMessage(username, callback) {
+    if(username === null) {
+        return;
+    }
+
+    var $$ = Framework7.$;
+    var history_url = "http://" + document.location.hostname + ":" + socket_port + '/message/' + username;
+    
+    // Ajax
+    $$.get(history_url, null, function (data) {
+        callback(data);
+    });
 }
